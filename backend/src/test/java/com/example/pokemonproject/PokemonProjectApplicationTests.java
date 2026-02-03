119f25dfc38b96eeb85cafb61949e57ab4efa097
nosou <nosoul125@gmail.com>
2026-02-03 06:05:00 -0500

    Initial backend setup (Spring Boot, Java 17)

---
+++ backend/src/test/java/com/example/pokemonproject/PokemonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokemonproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
